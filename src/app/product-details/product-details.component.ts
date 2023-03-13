2c03e31f73f5df81439f2530006133b6c0441520
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-03-13 15:05:25 -0500

    2 files modified, 4 added

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+
+}
