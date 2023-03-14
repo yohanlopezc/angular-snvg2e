86794ed126e69c0bf4f16a485f0c8a1c852439a4
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-03-13 23:06:23 -0500

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,9 +11,21 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
   ) { }
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
 }
